aa5b03129296502021152a8eb7bf0694b4ab313f
Olga Batiunia <olgabatyunya@gmail.com>
2023-09-30 22:46:52 +0200

    Initial Commit

---
+++ do it now./do_it_now_App.swift
@@ -0,0 +1,17 @@
+//
+//  do_it_now_App.swift
+//  do it now.
+//
+//  Created by Olga Batiunia on 30.09.23.
+//
+
+import SwiftUI
+
+@main
+struct do_it_now_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
